0a3a51b9efbb1507af8cfbd6afd75cbd57cc39cb
EgoisticallyEvil <127659686+Ego174@users.noreply.github.com>
2026-07-09 03:45:18 +0500

    Инициализация репозитория

---
+++ app/src/main.c
@@ -0,0 +1,17 @@
+/*
+main.c - главный модуль программы. 
+
+Хаиров Егор Вадимович
+МК-101
+*/
+
+#include "CReplace.h"
+
+#include <stdlib.h>
+
+
+int main (int argc, char *argv[], char *envp[]) {
+
+	return 0;
+
+}
