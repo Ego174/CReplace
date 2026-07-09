7c23ae12702540951a3a542d16c3367b8012f97d
EgoisticallyEvil <127659686+Ego174@users.noreply.github.com>
2026-07-09 23:46:30 +0500

    test_app_1
    Написаны тесты для приложения (Повторяют тесты CReplace но для файлов больших размеров)

--- app/tests/test_integration/test_app.c
+++ app/tests/test_integration/test_app.c
@@ -17,7 +17,56 @@ void tearDown(void) {
     // Вызывается после каждого теста (можно оставить пустым)
 }
 
+void test1(void) {
+    FILE* input = fopen("test.txt", "r");
+    FILE* output = fopen("output1.txt", "w");
+    replace(input, output, "0x414141", "0x30");
+    fclose(input);
+    fclose(output);
+    FILE* read = fopen("output1.txt", "r");
+    char must[35] = "000000000000000000000000000000000";
+    char res[35];
+    fread(res, 1, 35, read);
+    TEST_ASSERT_TRUE(memcmp(must, res, 35) == 0);
+    fclose(read);
+}
+
+void test2(void) {
+    FILE* input = fopen("test.txt", "r");
+    FILE* output = fopen("output2.txt", "w");
+    replace(input, output, "0x42", "0x30");
+    fclose(input);
+    fclose(output);
+    FILE* read = fopen("output2.txt", "r");
+    char must[100] = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+    char res[100];
+    fread(res, 1, 100, read);
+    TEST_ASSERT_TRUE(memcmp(must, res, 100) == 0);
+    fclose(read);
+}
+
+void test3(void) {
+    FILE* input = fopen("test.txt", "r");
+    FILE* output = fopen("output3.txt", "w");
+    replace(input, output, "0x00", "0x10");
+    fclose(input);
+    fclose(output);
+    FILE* read = fopen("output3.txt", "r");
+    char must[100] = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n";
+    char res[100];
+    fread(res, 1, 100, read);
+    TEST_ASSERT_TRUE(memcmp(must, res, 100) == 0);
+    fclose(read);
+}
+
 int main(void) {
     UNITY_BEGIN();
+    FILE* test = fopen("test.txt", "w");
+    fwrite("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", 1, 100, test);
+    fflush(test);
+    fclose(test);
+    RUN_TEST(test1);
+    RUN_TEST(test2);
+    RUN_TEST(test3);
     return UNITY_END();
 }
