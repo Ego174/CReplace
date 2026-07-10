63bd8986c7a6af3e59c00a1ee8902ef1cf410ad1
Хаиров Егор <hairovhairoff@yandex.ru>
2026-07-10 17:13:03 +0500

    Fixed htoc tests

--- lib/tests/test_hex_read/test_hex.c
+++ lib/tests/test_hex_read/test_hex.c
@@ -17,12 +17,32 @@ void tearDown(void) {
     // Вызывается после каждого теста (можно оставить пустым)
 }
 
+void test1(void) {
+    TEST_ASSERT_EQUAL_INT(0x61, htoc("61"));
+}
+
+void test2(void) {
+    TEST_ASSERT_EQUAL_INT(0x00, htoc("00"));
+}
+
+void test3(void) {
+    TEST_ASSERT_EQUAL_INT(0x0a, htoc("0a"));
+}
+
+void test4(void) {
+    TEST_ASSERT_EQUAL_INT(0xa1, htoc("a1"));
+}
+
+void test5(void) {
+    TEST_ASSERT_EQUAL_INT(0xaa, htoc("aa"));
+}
+
 int main(void) {
     UNITY_BEGIN();
-    TEST_ASSERT_EQUAL_INT(0x61, htoc("61"));
-    TEST_ASSERT_EQUAL_INT(0x00, htoc("00"));
-    TEST_ASSERT_EQUAL_INT(0x0a, htoc("0a"));
-    TEST_ASSERT_EQUAL_INT(0xa1, htoc("A1"));
-    TEST_ASSERT_EQUAL_INT(0xaa, htoc("aA"));
+    RUN_TEST(test1);
+    RUN_TEST(test2);
+    RUN_TEST(test3);
+    RUN_TEST(test4);
+    RUN_TEST(test5);
     return UNITY_END();
 }
