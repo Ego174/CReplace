db5a3a085fb15058bacba6c44159b715961f45b9
EgoisticallyEvil <127659686+Ego174@users.noreply.github.com>
2026-07-11 02:52:38 +0500

    Update CReplace.c
    Обновлен модуль CReplace для соответствия требованиям

--- lib/src/CReplace.c
+++ lib/src/CReplace.c
@@ -8,54 +8,47 @@ CReplace.c - главный модуль библиотеки.
 #include "CReplace.h"
 #include "hex.h"
 
-// Запись буфера в выходной файл
-#define writeBuf() \
-fwrite(buf, 1, ind, output); \
-ind = 0
-
-// Запись замены в выходной файл
-#define writeReplace() \
-fwrite(replaceS, 1, replace_len, output); \
-ind = 0
+// Обработка буфера
+#define checkBuf() \
+for(size_t i = 0; i < bytesRead; ++i) { \
+    if(buf[i] == searchS[matches]) ++matches; \
+    else { \
+        fwrite(searchS, 1, matches, output); \
+        fwrite(buf + i, 1, 1, output); \
+        matches = 0; \
+    } \
+    if(matches == searchLen) { \
+        fwrite(replaceS, 1, replaceLen, output); \
+        matches = 0; \
+    } \
+}
 
 // Заменяет вхождения одной последовательности байт на другую и записывает результат в выходной файл.
 void replace(FILE* input, FILE* output, char* search, char* replace) {
 
     char buf[N];
-    size_t ind = 0, search_len = strlen(search) / 2 - 1, replace_len = strlen(replace) / 2 - 1;
-
-    if(search_len > N) {
-        printf("Искомая последовательность больше буфера!\n");
-        exit(1);
-    }
+    size_t bytesRead, matches = 0, searchLen = strlen(search) / 2 - 1, replaceLen = strlen(replace) / 2 - 1;
 
     // Запись искомой последовательности байт
-    char* searchS = (char*)malloc(search_len);
+    char* searchS = (char*)malloc(searchLen);
     if(!searchS) {
         printf("Не удалось создать буфер для искомой последовательности!\n");
         exit(1);
     }
-    for(int i = 1; i <= search_len; ++i) *(searchS + i - 1) = htoc(search + 2 * i);
+    for(int i = 1; i <= searchLen; ++i) *(searchS + i - 1) = htoc(search + 2 * i);
 
     // Запись последовательности байт для замены
-    char* replaceS = (char*)malloc(replace_len);
+    char* replaceS = (char*)malloc(replaceLen);
     if(!searchS) {
         printf("Не удалось создать буфер для последовательности замены!\n");
         exit(1);
     }
-    for(int i = 1; i <= replace_len; ++i) *(replaceS + i - 1) = htoc(replace + 2 * i);
+    for(int i = 1; i <= replaceLen; ++i) *(replaceS + i - 1) = htoc(replace + 2 * i);
 
-    while(1) {
-        buf[ind] = getc(input);
-        if(buf[ind] == EOF) {
-            writeBuf();
-            break;
-        } else if(buf[ind] != searchS[ind++]) {
-            writeBuf();
-        } else if(ind == search_len) {
-            writeReplace();
-        }
+    while((bytesRead = fread(buf, 1, N, input)) == N) {
+        checkBuf()
     }
+    checkBuf()
 
     fflush(output);
     free(searchS);
