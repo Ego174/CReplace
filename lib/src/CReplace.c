49a439a039d4be66c2421f85864a2e18e81f5e1c
EgoisticallyEvil <127659686+Ego174@users.noreply.github.com>
2026-07-13 19:35:30 +0500

    Переписана логика, теперь модуль работает четко по условию

--- lib/src/CReplace.c
+++ lib/src/CReplace.c
@@ -9,20 +9,36 @@ CReplace.c - главный модуль библиотеки.
 #include "hex.h"
 
 // Обработка буфера
-void checkBuf(char* buf, size_t bufLen, char* searchS, size_t searchLen, size_t* matches, FILE* output, char* replaceS, size_t replaceLen, size_t pos) {
+void checkBuf(char* buf, size_t bufLen, char* searchS, size_t searchLen, size_t* matches, FILE* output, char* replaceS, size_t replaceLen) {
+    
     for(size_t i = 0; i < bufLen; ++i) {
+
+        // Совпадение, прибавляем счетчки и выводим замену если последовательность полностью совпала
         if(buf[i] == searchS[*matches]) {
             if(++(*matches) == searchLen) {
                 fwrite(replaceS, 1, replaceLen, output);
                 *matches = 0;
             }
-        } else {
+            continue;
+        }
+        
+        // Несовпадение, ищем максимальную подстроку которая может оказаться искомой последовательностью
+        if(*matches > 0) {
             size_t tmp = *matches;
             *matches = 0;
-            if(tmp > 1) checkBuf(searchS + pos + 1, tmp - 1, searchS, searchLen, matches, output, replaceS, replaceLen, pos + 1);
-            if(pos != 0) break;
-            
+            for(size_t k = 1; k <= tmp; ++k) {
+                fwrite(&searchS[k - 1], 1, 1, output);
+                size_t cur = tmp - k;
+                if(memcmp(searchS, searchS + k, cur) == 0 && buf[i] == searchS[cur]) {
+                    *matches = cur + 1; // Подстрока найдена
+                    break;
+                }
+            }
         }
+
+        // Полное несовпадение
+        if(*matches == 0) fwrite(&buf[i], 1, 1, output);
+
     }
 }
 
@@ -38,7 +54,7 @@ void replace(FILE* input, FILE* output, char* search, char* replace) {
         printf("Failed allocating memory for bytes to replace!\n");
         exit(1);
     }
-    for(size_t i = 1; i <= searchLen; ++i) *(searchS + i - 1) = htoc(search + 2 * i);
+    for(size_t i = 1; i <= searchLen; ++i) searchS[i-1] = htoc(search + 2 * i);
 
     // Запись последовательности байт для замены
     char* replaceS = (char*)malloc(replaceLen);
@@ -46,12 +62,11 @@ void replace(FILE* input, FILE* output, char* search, char* replace) {
         printf("Failed allocating memory for bytes to replace with!\n");
         exit(1);
     }
-    for(size_t i = 1; i <= replaceLen; ++i) *(replaceS + i - 1) = htoc(replace + 2 * i);
+    for(size_t i = 1; i <= replaceLen; ++i) replaceS[i-1] = htoc(replace + 2 * i);
 
     // Чтение файла в буфер, обработка буфера пока файл не закончится
-    while((bytesRead = fread(buf, 1, N, input)) > 0) {
-        for(size_t i = 0; i < bytesRead; ++i ) checkBuf();
-    }
+    while((bytesRead = fread(buf, 1, N, input)) > 0)
+        checkBuf(buf, bytesRead, searchS, searchLen, &matches, output, replaceS, replaceLen);
 
     // Вывод остатков совпавших байтов
     if(matches > 0) fwrite(searchS, 1, matches, output);
