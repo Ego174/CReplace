da08336e255f2d3b0cb43bcffa7154c250c3d509
EgoisticallyEvil <127659686+Ego174@users.noreply.github.com>
2026-07-14 01:13:35 +0500

    Дописал проверку ввода хекса

--- lib/src/hex.c
+++ lib/src/hex.c
@@ -5,12 +5,23 @@ hex.c - модуль для чтения hex строк.
 МК-101
 */
 
+#include <stdio.h>
 #include "hex.h"
 
 // Перевод hex-байта записанного в строке в char
 unsigned char htoc(char* hex) {
 
     unsigned char ret = 0, c0 = hex[0], c1 = hex[1];
+
+    if(!((c0 >= 0x30 && c0 <= 0x39) || (c0 >= 0x41 && c0 <= 0x46) || (c0 >= 0x61 && c0 <= 0x66))) {
+        printf("Not hex entered!\n");
+        exit(1);
+    }
+
+    if(!((c1 >= 0x30 && c1 <= 0x39) || (c1 >= 0x41 && c1 <= 0x46) || (c1 >= 0x61 && c1 <= 0x66))) {
+        printf("Not hex entered!\n");
+        exit(1);
+    }
 
     if(c1 < 0x41) ret += c1 - 0x30;
     else if(c1 < 0x61) ret += c1 - 0x37;
